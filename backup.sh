8b67d3cd7375e3e91a823a3e420769b8810c065d
mohu <bunbunyahho@hotmail.co.jp>
2018-04-27 13:31:01 +0900

    add MIT License

--- backup.sh
+++ backup.sh
@@ -6,3 +6,7 @@ USERNAME=$(whoami)
 sudo chown $USERNAME ~/secure
 
 tar -zcvf backup_secure.`date "+%Y%m%d_%H%M%S"`.tar.gz secure
+
+# Copyright (c) 2018 garbet
+# Released under the MIT license
+# https://github.com/garbet/backup_script/blob/master/LICENSE
